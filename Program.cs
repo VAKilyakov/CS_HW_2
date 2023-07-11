225aa7134e9b4b9c46072f14f5c31993b9d4876a
VAKilyakov <vakilyakov@bk.ru>
2023-07-11 22:23:28 +0300

    Task 3 is done v1

--- Program.cs
+++ Program.cs
@@ -87,3 +87,57 @@ PS D:\Home_Works\CS_HW_2> dotnet run
 Enter number 32679
 The third digit is: 6
 */
+
+/* Задача 3
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+/* Решение 3
+bool Weekend (int Number)
+{
+    bool Result = false;
+    if (Number >=6)
+    {
+        Result = true;
+    }
+
+       return Result; 
+}
+
+Console.Write("Enter number day of week ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0 || number >7)
+{
+    Console.Write("You number is'n day of week");
+}
+else
+{
+    if (Weekend(number))
+    {
+        Console.Write("Yes day is weekend");    
+    }
+    else
+    {
+        Console.Write("No day is't weekend");
+    }
+}
+*/
+
+/* Тесты 3
+PS D:\Home_Works\CS_HW_2> dotnet run
+Enter number day of week 8
+You number is'n day of week
+PS D:\Home_Works\CS_HW_2> dotnet run
+Enter number day of week 6
+Yes day is weekend
+PS D:\Home_Works\CS_HW_2> dotnet run
+Enter number day of week 7
+Yes day is weekend
+PS D:\Home_Works\CS_HW_2> dotnet run
+Enter number day of week 1
+No day is't weekend
+*/
